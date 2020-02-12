a610490c7ae8017b735506b02a11e3e0190c3647
lwpo2008 <lwpo2008@qq.com>
2020-02-12 15:04:58 +0800

    修正了同时访问造成的冲突

--- www/app.py
+++ www/app.py
@@ -28,17 +28,34 @@ def get_data_from_db():
 
 def get_variable_data_from_meter(room):
 	#等待串口空闲
-	while wm.ser.isOpen():
-		time.sleep(1)
-	return wm.achieve_variable_data(room)
+	if wm.ser.isOpen():
+		return 	{'room':room,
+				'number':-1.00,
+				'meter_date':'none',
+				'meter_time':'none',
+				'watt':-1.00,
+				'prev_watt':-1.00,
+				'frezon_active_power':-1.00,
+				'frezon_time':'none',
+				'active_power':-1.00,
+				'reactive_power':-1.00,
+				'cos':-1.00,
+				'a_v':-1.00,
+				'a_i':-1.00,
+				'a_z':-1.00,
+				'temp':-1.00,
+				'time':'接口正在使用，请稍后再试！'}
+	else:
+		return wm.achieve_variable_data(room)
 
 def update_time():
 	#等待串口空闲
-	while wm.ser.isOpen():
-		time.sleep(1)
-	#更新时间
-	wm.broadcasting_time()
-	return '已更新电表时间'
+	if wm.ser.isOpen():
+		return '接口正在使用，请稍后再试！'
+	else:
+		#更新时间
+		wm.broadcasting_time()
+		return '已更新电表时间'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
